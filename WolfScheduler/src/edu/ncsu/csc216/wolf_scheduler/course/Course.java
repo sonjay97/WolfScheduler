44672421ad6e5060877b5baf779bf99928184a8c
jsshah <jsshah@ncsu.edu>
2022-09-05 11:43:11 -0400

    "[Skeleton] Submitting a skeleton of WolfScheduler"

---
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Course.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.wolf_scheduler.course;
+
+/**
+ * @author jayshah
+ *
+ */
+public class Course {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
