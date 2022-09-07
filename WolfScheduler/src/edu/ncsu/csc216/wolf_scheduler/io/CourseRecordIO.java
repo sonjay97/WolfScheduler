b05642a9fec54f0bbe83b1b0e73e67d99fa0892a
jsshah <jsshah@ncsu.edu>
2022-09-07 10:10:12 -0400

    [Implementation] Added code to CourseRecordIO

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
@@ -3,41 +3,93 @@
  */
 package edu.ncsu.csc216.wolf_scheduler.io;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import edu.ncsu.csc216.wolf_scheduler.course.Course;
 
 /**
- * Reads Course records from text files. Writes a set of CourseRecords to a file.
+ * Reads Course records from text files. Writes a set of CourseRecords to a
+ * file.
  * 
  * @author jayshah
  */
 public class CourseRecordIO {
-	
-    /**
-     * Reads course records from a file and generates a list of valid Courses.  Any invalid
-     * Courses are ignored.  If the file to read cannot be found or the permissions are incorrect
-     * a File NotFoundException is thrown.
-     * @param fileName file to read Course records from
-     * @return a list of valid Courses
-     * @throws FileNotFoundException if the file cannot be found or read
-     */
-    public static ArrayList<Course> readCourseRecords(String fileName) throws FileNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
-    /**
-     * Writes the given list of Courses to 
-     * @param fileName file to write schedule of Courses to
-     * @param courses list of Courses to write
-     * @throws IOException if cannot write to file
-     */
-    public static void writeCourseRecords(String fileName, ArrayList<Course> courses) throws IOException {
-        // TODO Auto-generated method stub
-        
-    }
+	/**
+	 * Reads course records from a file and generates a list of valid Courses. Any
+	 * invalid Courses are ignored. If the file to read cannot be found or the
+	 * permissions are incorrect a File NotFoundException is thrown.
+	 * 
+	 * @param fileName file to read Course records from
+	 * @return a list of valid Courses
+	 * @throws FileNotFoundException if the file cannot be found or read
+	 */
+	public static ArrayList<Course> readCourseRecords(String fileName) throws FileNotFoundException {
+		Scanner fileReader = new Scanner(new FileInputStream(fileName)); // Create a file scanner to read the file
+		ArrayList<Course> courses = new ArrayList<Course>(); // Create an empty array of Course objects
+		while (fileReader.hasNextLine()) { // While we have more lines in the file
+			try { // Attempt to do the following
+					// Read the line, process it in readCourse, and get the object
+					// If trying to construct a Course in readCourse() results in an exception, flow
+					// of control will transfer to the catch block, below
+				Course course = readCourse(fileReader.nextLine());
+
+				// Create a flag to see if the newly created Course is a duplicate of something
+				// already in the list
+				boolean duplicate = false;
+				// Look at all the courses in our list
+				for (int i = 0; i < courses.size(); i++) {
+					// Get the course at index i
+					Course current = courses.get(i);
+					// Check if the name and section are the same
+					if (course.getName().equals(current.getName())
+							&& course.getSection().equals(current.getSection())) {
+						// It's a duplicate!
+						duplicate = true;
+						break; // We can break out of the loop, no need to continue searching
+					}
+				}
+				// If the course is NOT a duplicate
+				if (!duplicate) {
+					courses.add(course); // Add to the ArrayList!
+				} // Otherwise ignore
+			} catch (IllegalArgumentException e) {
+				// The line is invalid b/c we couldn't create a course, skip it!
+			}
+		}
+		// Close the Scanner b/c we're responsible with our file handles
+		fileReader.close();
+		// Return the ArrayList with all the courses we read!
+		return courses;
+	}
+
+	private static Course readCourse(String nextLine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Writes the given list of Courses to
+	 * 
+	 * @param fileName file to write schedule of Courses to
+	 * @param courses  list of Courses to write
+	 * @throws IOException if cannot write to file
+	 */
+	public static void writeCourseRecords(String fileName, ArrayList<Course> courses) throws IOException {
+		PrintStream fileWriter = new PrintStream(new File(fileName));
+
+		for (int i = 0; i < courses.size(); i++) {
+			fileWriter.println(courses.get(i).toString());
+		}
+
+		fileWriter.close();
+
+	}
 }
